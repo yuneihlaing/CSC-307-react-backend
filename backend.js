ad8fe75bb60682517dc29b85afa50f7309ff0000
Yun Ei Hlaing <yuneihlaing@gmail.com>
2022-04-04 13:22:45 -0700

    Get users by Id

--- backend.js
+++ backend.js
@@ -5,49 +5,62 @@ const port = 5000;
 app.use(express.json());
 
 const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor",
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ],
+    users_list: [{
+            id: "xyz789",
+            name: "Charlie",
+            job: "Janitor",
+        },
+        {
+            id: "abc123",
+            name: "Mac",
+            job: "Bouncer",
+        },
+        {
+            id: "ppp222",
+            name: "Mac",
+            job: "Professor",
+        },
+        {
+            id: "yat999",
+            name: "Dee",
+            job: "Aspring actress",
+        },
+        {
+            id: "zap555",
+            name: "Dennis",
+            job: "Bartender",
+        },
+    ],
 };
 
-app.get('/users', (req, res) => {
+app.get("/users", (req, res) => {
     const name = req.query.name;
-    if (name != undefined){
+    if (name != undefined) {
         let result = findUserByName(name);
-        result = {users_list: result};
+        result = { users_list: result };
         res.send(result);
-    }
-    else{
+    } else {
         res.send(users);
     }
 });
 
-const findUserByName = (name) => { 
-    return users['users_list'].filter( (user) => user['name'] === name); 
+const findUserByName = (name) => {
+    return users["users_list"].filter((user) => user["name"] === name);
+};
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"];
+    let result = findUserById(id);
+    if (result == undefined || result.length == 0)
+        res.status(404).send("Resource not found.");
+    else {
+        result = { users_list: result };
+        res.send(result);
+    }
+});
+
+function findUserById(id) {
+    return users["users_list"].find((user) => user["id"] === id);
 }
 
 app.listen(port, () => {
