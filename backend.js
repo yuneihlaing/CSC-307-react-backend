3a08bfcf75effe6c225e09c975faad53ba2dfaa5
Yun Ei Hlaing <yuneihlaing@gmail.com>
2022-04-04 13:08:48 -0700

    Getting users by name

--- backend.js
+++ backend.js
@@ -1,13 +1,55 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
+
+app.get('/users', (req, res) => {
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
 });
+
+const findUserByName = (name) => { 
+    return users['users_list'].filter( (user) => user['name'] === name); 
+}
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-});      
+});
